4f73033f87f6c2a460ad62cba814b0a01d47a6f3
himanshushah15 <rockstarmarwari@gmail.com>
2021-01-24 01:48:06 +0100

    added input parser

---
+++ input_handler.py
@@ -0,0 +1,26 @@
+import io 
+from pizza import Pizza
+
+#read the input file 
+input_file = open("Problems/2021/practice_problem/e_many_teams.in")
+
+# parse the num of pizza and team details from the first line 
+temp = input_file.readline()
+temp_list = temp.split()
+num_of_pizza = int(temp_list[0])
+num_of_t2 = temp_list[1]
+num_of_t3 = temp_list[2]
+num_of_t4 = temp_list[3]
+
+# now get the pizza information and store in the pizza class
+pizza_list = []
+for index in range(num_of_pizza):
+    temp = input_file.readline()
+    temp_list = temp.split()
+    temp_list.remove(temp_list[0])
+    pizza_list.append(Pizza(index, temp_list))
+
+#close the file and print the number of pizzas parsed
+input_file.close()
+if (len(pizza_list) == num_of_pizza):
+    print("parse successful. Parsed " + str(len(pizza_list)) + " pizzas")
---
+++ pizza.py
@@ -0,0 +1,22 @@
+class Pizza:
+
+    def __init__(self, index, ingredients_list):
+        self.index = index
+        self.num_of_ingredients = len(ingredients_list)
+        self.ingredients = ingredients_list
+
+    
+    def get_ingredients(self):
+        return self.ingredients
+    
+    def get_index(self):
+        return self.index
+
+
+# def main():
+#     pizza = Pizza(0, ["Onion", "Tomato", "Mozarella"])
+#     print(pizza.get_index())
+#     print(pizza.get_ingredients())
+
+# if __name__ == "__main__":
+#     main()
